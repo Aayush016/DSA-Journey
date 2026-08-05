b5b6b59371df2e6099b71f487d19c82646a88377
Aayush Pandey <aayushpandey016@gmail.com>
2026-08-05 19:04:23 +0530

    Add new Backtracking Folder and write 1st code

---
+++ Backtracking/Btkg_arrays.java
@@ -0,0 +1,28 @@
+public class Btkg_arrays {
+
+    public static void changeArr(int arr[], int i, int val){
+        // Base Case
+        if(i==arr.length){
+            printArr(arr); // this line prints the first array that is 12345
+            return;
+        }
+
+        // Recursion
+        arr[i] = val;
+        changeArr(arr, i+1, val+1);
+        arr[i] = arr[i] - 2; // Backtracking Step
+    }
+
+    public static void printArr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        changeArr(arr, 0, 1);
+        printArr(arr);
+    }
+}
