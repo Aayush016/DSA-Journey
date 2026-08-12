2d5e7082c89ee41ebbd165ef6f5e137086e8a9c2
Aayush Pandey <aayushpandey016@gmail.com>
2026-08-12 16:59:42 +0530

    Code for Remove, reverse, checkPalindrome added

--- Linked List/Basics_LL.java
+++ Linked List/Basics_LL.java
@@ -1,10 +1,10 @@
 public class Basics_LL {
 
-    public static class Node{
+    public static class Node {
         int data;
         Node next;
 
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
             this.next = null;
         }
@@ -17,10 +17,10 @@ public class Basics_LL {
     // -------------------- ADD in a Linked List --------------------
 
     // Add Node at First
-    public void addFirst(int data){
+    public void addFirst(int data) {
         Node newNode = new Node(data);
         size++;
-        if(head == null){
+        if (head == null) {
             head = tail = newNode;
             return;
         }
@@ -29,10 +29,10 @@ public class Basics_LL {
     }
 
     // Add Node at last
-    public void addLast(int data){
+    public void addLast(int data) {
         Node newNode = new Node(data);
         size++;
-        if(head == null){
+        if (head == null) {
             head = tail = newNode;
             return;
         }
@@ -41,8 +41,8 @@ public class Basics_LL {
     }
 
     // Add int the middle
-    public void add(int idx, int data){
-        if(idx == 0){
+    public void add(int idx, int data) {
+        if (idx == 0) {
             addFirst(data);
             return;
         }
@@ -50,7 +50,7 @@ public class Basics_LL {
         size++;
         Node temp = head;
         int i = 0;
-        while(i < idx-1){
+        while (i < idx - 1) {
             temp = temp.next;
             i++;
         }
@@ -61,15 +61,15 @@ public class Basics_LL {
     }
 
     // Print a Linked List
-    public void print(){
-        if(head == null){
+    public void print() {
+        if (head == null) {
             System.out.println("LL is Empty");
             return;
         }
         Node temp = head;
-        while(temp != null){
+        while (temp != null) {
             System.out.print(temp.data + " -> ");
-            temp=temp.next;
+            temp = temp.next;
         }
         System.out.println("null");
     }
@@ -77,13 +77,13 @@ public class Basics_LL {
     // -------------------- REMOVE in a Linked List --------------------
 
     // Remove First
-    public int removeFirst(){
-        if(size == 0){
+    public int removeFirst() {
+        if (size == 0) {
             System.out.println("LL is empty");
             return Integer.MIN_VALUE;
-        } else if(size == 1){
+        } else if (size == 1) {
             int val = head.data;
-            head=tail=null;
+            head = tail = null;
             size = 0;
             return val;
         }
@@ -95,10 +95,10 @@ public class Basics_LL {
 
     // Remove Last
     public int removeLast() {
-        if(size == 0){
+        if (size == 0) {
             System.out.println("LL is empty");
             return Integer.MIN_VALUE;
-        } else if(size == 1){
+        } else if (size == 1) {
             int val = head.data;
             head = tail = null;
             size = 0;
@@ -107,7 +107,7 @@ public class Basics_LL {
 
         // prev = size-2
         Node prev = head;
-        for (int i = 0; i < size-2; i++) {
+        for (int i = 0; i < size - 2; i++) {
             prev = prev.next;
         }
 
@@ -116,6 +116,135 @@ public class Basics_LL {
         tail = prev;
         size--;
         return val;
+    }
+
+    // -------------------- Search for a key in a Linked List --------------------
+
+    // Iterative Way
+    public int itrSearch(int key) {
+        Node temp = head;
+        int i = 0;
+
+        while (temp != null) {
+            if (temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+
+        // Key not found
+        return -1;
+    }
+
+    // Recursive Way
+    public int helper(Node head, int key) {
+        if (head == null) {
+            return -1;
+        }
+
+        if (head.data == key) {
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if (idx == -1) {
+            return -1;
+        }
+        return idx + 1;
+    }
+
+    public int recSearch(int key) {
+        return helper(head, key);
+    }
+
+    // -------------------- Reverse a Linked List --------------------
+
+    public void reverse() {
+        Node prev = null;
+        Node curr = tail = head; // Tail and curr stores head value;
+        Node next;
+
+        while (curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        head = prev;
+    }
+
+    public void deleteNthfromEnd( int n){
+        // Calculate the size of hte ll
+        int sz = 0;
+        Node temp = head;
+        while(temp != null){
+            temp = temp.next;
+            sz++;
+        }
+        if(n == sz){
+            head = head.next; // Remove First element
+            return;
+        }
+
+        int i = 1;
+        int iToFind = sz-n;
+        Node prev = head;
+        while(i < iToFind){
+            prev = prev.next;
+            i++;
+        }
+
+        prev.next = prev.next.next;
+        return;
+
+    }
+
+    // Slow-Fast Approach
+    public Node findMid(Node head){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public boolean checkPalindrome() {
+        if(head == null || head.next == null){
+            return true;
+        }
+        // Step-1: Find Mid
+        Node midNode = findMid(head);
+
+        // Step-2: Reverse 2nd half
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node right = prev;
+        Node left = head;
+
+        // Step-3: Check left & right
+        while(right != null){
+            if(left.data != right.data){
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
@@ -131,15 +260,23 @@ public class Basics_LL {
         ll.addLast(4);
         // ll.print();
 
-        ll.addLast(5);
+        ll.addLast(2);
 
-        ll.add(2, 3);
+        ll.add(4, 1);
         ll.print();
-        ll.removeFirst();
-        ll.print();
+        // ll.removeFirst();
+        // ll.print();
 
-        ll.removeLast();
-        ll.print();
+        // ll.removeLast();
+        // ll.print();
+
+        // System.out.println(ll.recSearch(3));
+        // System.out.println(ll.recSearch(10));
+
+        // ll.reverse();
+        // ll.deleteNthfromEnd(2);
+        System.out.println(ll.checkPalindrome());
+        // ll.print();
 
     }
 }
